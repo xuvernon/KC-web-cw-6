08720262935e2cf6df5ac39b72d76c2ae5c6b919
sarah almutari <sara0moha78@gmail.com>
2022-08-16 22:48:37 +0300

    .

--- java.js
+++ java.js
@@ -5,19 +5,19 @@ let grade = prompt('enter your grade in numbers')
 console.log(grade)
 
 if(grade <=100 && grade >=90 ){
-   console.log(" %cyou got an A+🥳", 'color: green; background: yellow; font-size: 30px')
+   console.log(" %cyou got an A+🥳", 'color: pink;  font-size: 30px')
 } 
 else if( grade >= 80 && grade < 90 ){
-    console.log(" %cyou got a B+ 🤩" ,'color: green; background: yellow; font-size: 30px')
+    console.log(" %cyou got a B+ 🤩" ,'color: green; font-size: 30px')
 }
 else if( grade >= 70 && grade < 80){
-    console.log(" %cyou got a C+ 🙂",'color: green; background: yellow; font-size: 30px')
+    console.log(" %cyou got a C+ 🙂",'color: blue; font-size: 30px')
 }
 else if( grade >= 60 && grade < 70){
-    console.log('%cyou got a D+ 😕', 'color: green; background: yellow; font-size: 30px')
+    console.log('%cyou got a D+ 😕', 'color: yellow ;font-size: 30px')
 }
 else if( grade >= 50 && grade < 60 ){
     console.log
-    ('%cyou got a F 💔', 'color: green; background: yellow; font-size: 30px');
+    ('%cyou got a F 💔', 'color: red; font-size: 30px');
         
 }
